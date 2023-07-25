80ce621fffcdb8ca529f6d4b5941a28bfc802a16
johnson <you@example.com>
2023-07-26 00:17:13 +0800

    router-updata

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,52 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+const routes =[
+  {
+    path: "/",
+    component:()=>import("../FrontLayout.vue"),
+    children:[
+      {
+      path :"",
+      name:"Home",
+      component:()=>import("../views/FontLayout/HomeView.vue"),
+      children:[]
+      },
+      {
+        path:"Cart",
+        name:"Cart",
+        component:()=>import("../views/FontLayout/CartView.vue")
+      },
+      {
+        path:"Keep",
+        name:"Keep",
+        component:()=>import("../views/FontLayout/KeepView.vue")
+      },
+      {
+        path:"Stand",
+        name:"Stand",
+        component:()=>import("../views/FontLayout/standProducts.vue")
+      },
+      {
+        path:"Check",
+        name:"Check",
+        component:()=>import("../views/FontLayout/CheckList.vue")
+      },
+      {
+        path:"Checkouts",
+        name:"Checkouts",
+        component:()=>import("../views/FontLayout/CheckoutsView.vue")
+      }
+    ]
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    component: () => import("../views/FontLayout/NotFound.vue")
+  },
+];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      component:()=>import("../FrontLayout.vue"),
-      children:[
-        {
-        path :"",
-        name:"Home",
-        component:()=>import("../views/FontLayout/HomeView.vue"),
-        children:[]
-        },
-        {
-          path:"Cart",
-          name:"Cart",
-          component:()=>import("../views/FontLayout/CartView.vue")
-        },
-        {
-          path:"Keep",
-          name:"Keep",
-          component:()=>import("../views/FontLayout/KeepView.vue")
-        },
-        {
-          path:"Stand",
-          name:"Stand",
-          component:()=>import("../views/FontLayout/standProducts.vue")
-        },
-        {
-          path:"Check",
-          name:"Check",
-          component:()=>import("../views/FontLayout/CheckList.vue")
-        },
-        {
-          path:"Checkouts",
-          name:"Checkouts",
-          component:()=>import("../views/FontLayout/CheckoutsView.vue")
-        }
-      ]
-    },
-    {
-      path: "/:pathMatch(.*)*",
-      component: () => import("../views/FontLayout/NotFound.vue")
-    },
-  ]
+  linkActiveClass: "active",
+  routes,
 });
 
 export default router;
